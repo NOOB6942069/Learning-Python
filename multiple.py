924190b88b083c7957cb7c5f582dc1e6e2f10280
Sarthak Guleria <117332046+NOOB6942069@users.noreply.github.com>
2023-07-18 00:56:52 +0530

    Add files via upload

---
+++ multiple.py
@@ -0,0 +1,27 @@
+# Multiple assignments allow us to assign multiple vriables in one line of code
+#lets take a bunch of variables below 
+#name = 'fren'
+#age = 22
+#smart = True 
+#now lets print these separately 
+#print(name)
+#print(age)
+#print(smart)
+#now lets print it in 1 line of code using multiple assignment 
+name , age , smart = 'fren', 22, True
+#now if we print it it should give us the same results.
+#print(name, age, smart)
+#lets try with 5 variables having same values 
+me = 30 
+she = 30 
+you = 30 
+them = 30 
+us =30 
+#now lets put it in line of code
+me = she = you = them = us = 30 
+print(me )
+print(she)
+print(us )
+print (them)
+#nciely prints all of them. 
+#chapter ends here.
