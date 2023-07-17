d972b79e36d20b4729577da2355513522fe4c9d1
Sarthak Guleria <117332046+NOOB6942069@users.noreply.github.com>
2023-07-18 01:24:20 +0530

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,12 @@
+name = "Bro Fren" # leta take this string and make some changes to it 
+print(len(name)) # this will print the length of the variable 
+print(name.find("f")) # this will tell us the place no where the F is located in the variable stringe
+print (name.capitalize()) # this will capitalise the output 
+print(name.upper()) #this will put all the alphabets in uppercase 
+print(name.lower())# this will print the outout in the lowercase
+print(name.isdigit())# this will tell if the variable is a digit 
+print(name.isalpha())# this will tell if the variable is a number
+print(name.count("r"))# can count the number of characters in the string
+print(name* 50) #this will print the name variable by 50 times
+print(name.replace("r","a"))#will replace the first variable with the 2nd one 
+print(name.find("F"))
